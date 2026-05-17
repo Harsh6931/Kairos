8f73bc5bf07f0033b03ff04cbbbd826dc50dee69
HARSHIT SINHA <harshitsinha2006@gmail.com>
2026-05-17 17:14:15 +0530

    Remove package.json file as part of project restructuring

--- apps/web/src/app/providers/AppProviders.tsx
+++
@@ -1,6 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-export function AppProviders({ children }: PropsWithChildren): JSX.Element {
-  return <>{children}</>;
-}
-
--- apps/web/src/shared/lib/classNames.ts
+++
@@ -1,6 +0,0 @@
-export function classNames(
-  ...tokens: Array<string | false | null | undefined>
-): string {
-  return tokens.filter(Boolean).join(" ");
-}
-
